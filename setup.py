bbbb6cf8d662f220f02140b988d4a9b8ebec2e0a
Erik Taubeneck <erik.taubeneck@gmail.com>
2013-06-26 12:47:21 -0400

    updated to use setuptools with requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ Flask-Twitter-OEmbedder
 
 Embedded tweets in Flask Jinja2 Templates with only the Tweet_ID
 """
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name = "Flask-Twitter-OEmbedder",
@@ -15,5 +15,28 @@ setup(
     author_email = 'erik.taubeneck@gmail.com',
     description = 'Embedded tweets in Flask Jinja2 Templates with only the Tweet_ID',
     py_modules = ['flask_twitter_oembedder'],
+    zip_safe=False,
     platforms = 'any',
+    include_package_data=True,
+    test_suite="tests",
+    install_requires=[
+        'Flask',
+        'Flask-Cache',
+        'requests',
+        'requests_oauthlib',
+        ],
+    tests_require=[
+        'Flask-Testing',
+        'Flask-Cache',
+        'httpretty',
+        ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: MIT',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
 )
