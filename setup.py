a41ac62c61599813c8b13a6b7f5b1f241bb16004
Erik Taubeneck <erik.taubeneck@gmail.com>
2013-12-06 18:49:02 -0500

    cache_keys: version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name = "Flask-Twitter-OEmbedder",
-    version = '0.1.6',
+    version = '0.1.7',
     url = 'https://github.com/eriktaubeneck/flask-twitter-oembedder',
     license = 'MIT',
     author = 'Erik Taubeneck',
