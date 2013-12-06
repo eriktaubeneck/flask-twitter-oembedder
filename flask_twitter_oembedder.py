87feed8e965ac49a63f7940605b42e1aab3b2e4e
Erik Taubeneck <erik.taubeneck@gmail.com>
2013-12-06 18:42:09 -0500

    cache_keys: rearrange caching decorator

--- flask_twitter_oembedder.py
+++ flask_twitter_oembedder.py
@@ -9,9 +9,6 @@ class TwitterOEmbedder(object):
             self.init(app, cache, debug)
 
     def init(self, app, cache, timeout=None, debug=None):
-        def _cache_key(func, *args, **kwargs):
-            return (args[0], kwargs.get('omit_script', False))
-
         twitter_timeout = 60*60*24*365
         if timeout > twitter_timeout:
             raise Exception("TwitterOEmbedder: Cache expiry should not exceed 1 year "
@@ -26,7 +23,6 @@ class TwitterOEmbedder(object):
 
         @app.context_processor
         def tweet_processor():
-            @cache.memoize(timeout=timeout)
             def oembed_tweet(tweet_id,
                              access_token=app.config['TWITTER_ACCESS_TOKEN'],
                              token_secret=app.config['TWITTER_TOKEN_SECRET'],
@@ -36,16 +32,19 @@ class TwitterOEmbedder(object):
                               access_token,
                               token_secret)
                 url = 'https://api.twitter.com/1.1/statuses/oembed.json'
-                params = {'id':tweet_id,
-                          'omit_script':omit_script}
-                r = requests.get(url, params=params, auth=auth)
-                try:
-                    tweet_html = Markup(r.json()[u'html'])
-                except KeyError as e:
-                    if debug or (debug is None and app.debug):
-                        raise e
-                    else:
-                        return ''
-                return tweet_html
-            oembed_tweet.make_cache_key = _cache_key
+
+                @cache.memoize(timeout=timeout)
+                def get_tweet_html(tweet_id, omit_script):
+                    params = {'id':tweet_id,
+                              'omit_script':omit_script}
+                    r = requests.get(url, params=params, auth=auth)
+                    try:
+                        tweet_html = Markup(r.json()[u'html'])
+                    except KeyError as e:
+                        if debug or (debug is None and app.debug):
+                            raise e
+                        else:
+                            return ''
+                    return tweet_html
+                return get_tweet_html(tweet_id, omit_script)
             return dict(oembed_tweet=oembed_tweet)
